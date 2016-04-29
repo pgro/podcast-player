0867ebb752cd3e95da7e60db362e5802b6e90472
pgro <pgro@users.noreply.github.com>
2016-04-29 20:20:19 +0200

    parse basic episode data from a given podcast xml url

---
+++ PodcastPlayer/Episode.swift
@@ -0,0 +1,15 @@
+//
+//  Episode.swift
+//  PodcastPlayer
+//
+//  Created by Peter Großmann on 29.04.16.
+//  Copyright © 2016 Peter Großmann. All rights reserved.
+//
+
+
+class Episode {
+    var title: String?
+    var description: String?
+    var url: String?
+    var date: String?
+}
