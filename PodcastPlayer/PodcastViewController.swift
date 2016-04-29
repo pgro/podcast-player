d37b6f145de2c24d0d572cc56a9a622148822b24
pgro <pgro@users.noreply.github.com>
2016-04-29 21:16:13 +0200

    extract podcast xml parser into own class

--- PodcastPlayer/PodcastViewController.swift
+++ PodcastPlayer/PodcastViewController.swift
@@ -8,76 +8,14 @@
 
 import UIKit
 
-class PodcastViewController: UIViewController, NSXMLParserDelegate {
+class PodcastViewController: UIViewController {
     var episodes = Array<Episode>()
-    var currentEpisode: Episode?
-    var currentElementName: String?
-    var parseTitle = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let podcastUrl = "http://www.rocketbeans.tv/plauschangriff.xml"
-        let parser = NSXMLParser(contentsOfURL: NSURL(string: podcastUrl)!)
-        parser?.delegate = self
-        parser?.parse()
+        let parser = PodcastXmlParser()
+        episodes = parser.parseEpisodes()
     }
     
-    
-    func parser(parser: NSXMLParser,
-                didStartElement elementName: String,
-                namespaceURI: String?,
-                qualifiedName qName: String?,
-                attributes attributeDict: [String : String]) {
-        currentElementName = elementName
-        
-        if elementName == "item" {
-            currentEpisode = Episode()
-            return
-        }
-        
-        
-    }
-
-    func parser(parser: NSXMLParser,
-                foundCharacters string: String) {
-        if currentElementName == nil {
-            return
-        }
-        
-        if currentEpisode != nil {
-            parseEpisode(string)
-        }
-    }
-    
-    func parseEpisode(string: String) {
-        switch currentElementName! {
-        case "title":
-            currentEpisode?.title += string
-            break
-        case "guid":
-            currentEpisode?.url += string
-            break
-        case "pubDate":
-            currentEpisode?.date += string
-            break
-        case "itunes:summary":
-            currentEpisode?.description += string
-        default:
-            break
-        }
-    }
-    
-    func parser(parser: NSXMLParser,
-                didEndElement elementName: String,
-                namespaceURI: String?,
-                qualifiedName qName: String?) {
-        if elementName == "item" {
-            episodes.append(currentEpisode!)
-            currentEpisode = nil
-        }
-        
-        currentElementName = nil
-    }
 }
-
---
+++ PodcastPlayer/PodcastXmlParser.swift
@@ -0,0 +1,82 @@
+//
+//  PodcastXmlParser.swift
+//  PodcastPlayer
+//
+//  Created by Peter Großmann on 29.04.16.
+//  Copyright © 2016 Peter Großmann. All rights reserved.
+//
+
+import Foundation
+
+class PodcastXmlParser: NSObject, NSXMLParserDelegate {
+    var episodes = Array<Episode>()
+    var currentEpisode: Episode?
+    var currentElementName: String?
+    
+    func parseEpisodes() -> Array<Episode> {
+        let podcastUrl = "http://www.rocketbeans.tv/plauschangriff.xml"
+        let parser = NSXMLParser(contentsOfURL: NSURL(string: podcastUrl)!)
+        parser?.delegate = self
+        parser?.parse()
+        return episodes
+    }
+    
+
+// MARK: - NSXMLParserDelegate
+    
+    func parser(parser: NSXMLParser,
+                didStartElement elementName: String,
+                                namespaceURI: String?,
+                                qualifiedName qName: String?,
+                                              attributes attributeDict: [String : String]) {
+        currentElementName = elementName
+        
+        if elementName == "item" {
+            currentEpisode = Episode()
+            return
+        }
+        
+        
+    }
+    
+    func parser(parser: NSXMLParser,
+                foundCharacters string: String) {
+        if currentElementName == nil {
+            return
+        }
+        
+        if currentEpisode != nil {
+            parseEpisode(string)
+        }
+    }
+    
+    func parseEpisode(string: String) {
+        switch currentElementName! {
+        case "title":
+            currentEpisode?.title += string
+            break
+        case "guid":
+            currentEpisode?.url += string
+            break
+        case "pubDate":
+            currentEpisode?.date += string
+            break
+        case "itunes:summary":
+            currentEpisode?.description += string
+        default:
+            break
+        }
+    }
+    
+    func parser(parser: NSXMLParser,
+                didEndElement elementName: String,
+                              namespaceURI: String?,
+                              qualifiedName qName: String?) {
+        if elementName == "item" {
+            episodes.append(currentEpisode!)
+            currentEpisode = nil
+        }
+        
+        currentElementName = nil
+    }
+}
