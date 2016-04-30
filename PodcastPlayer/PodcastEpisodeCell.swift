06f239d2d7d02ec5938a73336f9ecc0503fb9d0d
pgro <pgro@users.noreply.github.com>
2016-04-30 11:06:13 +0200

    show podcast episodes in collection view

---
+++ PodcastPlayer/PodcastEpisodeCell.swift
@@ -0,0 +1,16 @@
+//
+//  PodcastEpisodeCell.swift
+//  PodcastPlayer
+//
+//  Created by Peter Großmann on 30.04.16.
+//  Copyright © 2016 Peter Großmann. All rights reserved.
+//
+
+import UIKit
+
+class PodcastEpisodeCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+
+}
--- PodcastPlayer/PodcastViewController.swift
+++ PodcastPlayer/PodcastViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PodcastViewController: UIViewController {
+class PodcastViewController: UICollectionViewController {
     var episodes = Array<Episode>()
 
     override func viewDidLoad() {
@@ -18,4 +18,39 @@ class PodcastViewController: UIViewController {
         episodes = parser.parseEpisodes()
     }
     
+    override func viewWillTransitionToSize(size: CGSize,
+          withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        
+        collectionViewLayout.invalidateLayout()
+    }
+    
+    
+    //MARK: - UICollectionViewDataSource
+    
+    override func collectionView(collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView,
+                cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("episodeCell", forIndexPath: indexPath) as! PodcastEpisodeCell
+        
+        let episode = episodes[indexPath.item]
+        cell.titleLabel.text = episode.title
+        cell.descriptionLabel.text = episode.description
+        cell.dateLabel.text = episode.date
+        
+        return cell
+    }
+    
+    
+    // MARK: UICollectionViewDelegate
+    
+    func collectionView(collectionView: UICollectionView,
+            layout collectionViewLayout: UICollectionViewLayout,
+            sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(collectionView.bounds.size.width, CGFloat(85))
+    }
 }
