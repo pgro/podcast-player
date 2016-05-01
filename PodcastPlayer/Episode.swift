76a4ec3280bc52c47dcbdb81e955909ac90ebe01
pgro <pgro@users.noreply.github.com>
2016-05-01 18:39:35 +0200

    move episode download and status handling to respective class

--- PodcastPlayer/Episode.swift
+++ PodcastPlayer/Episode.swift
@@ -8,16 +8,27 @@
 
 import Foundation
 
+
+enum Status {
+    case NeedsDownload
+    case IsDownloading
+    case FinishedDownload
+}
+
 class Episode {
     var title = ""
     var description = ""
     var url = "" {
         didSet {
             prepareFileName()
+            status = prepareEpisodeFilePath().status == PathStatus.Exists
+                                                ? Status.FinishedDownload
+                                                : Status.NeedsDownload
         }
     }
     var date = ""
     var fileName = ""
+    var status = Status.NeedsDownload
     
     
     /** Retrieves the unique file name for the current url from user defaults.
@@ -40,4 +51,71 @@ class Episode {
         }
         fileName = episodesToFiles[url]!
     }
+    
+    
+    func download() {
+        if status == Status.IsDownloading {
+            // download is already in progress -> nothing to do
+            return
+        }
+        
+        /* download selected podcast episode (if necessary)
+         * and update the cell status accordingly */
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            let result = self.prepareEpisodeFilePath()
+            if result.status == PathStatus.Exists {
+                self.status = Status.FinishedDownload
+                return
+            }
+            if result.status == PathStatus.Error {
+                self.status = Status.NeedsDownload
+                return
+            }
+            
+            self.status = Status.IsDownloading
+            let episodeFileData = NSData(contentsOfURL: NSURL(string: self.url)!)
+            // TODO: avoid iCloud backup
+            let success = episodeFileData?.writeToFile(result.path!, atomically: true)
+            self.status = (success != nil) ? Status.FinishedDownload
+                                           : Status.NeedsDownload
+        }
+    }
+    
+
+    // MARK: - episode file path handling
+    
+    func folderPathForEpisodes() -> String {
+        var folderPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+        folderPath = folderPath.stringByAppendingString("/episodes")
+        return folderPath
+    }
+    
+    
+    enum PathStatus {
+        case Error
+        case Exists
+        case IsFree
+    }
+    
+    /** Returns the target path for download when it is available
+     or nil if it was already downloaded (i.e. file exists)
+     or if an error occured. */
+    func prepareEpisodeFilePath() -> (status: PathStatus, path: String?) {
+        let folderPath = folderPathForEpisodes()
+        let path = folderPath.stringByAppendingString("/" + fileName)
+        
+        if NSFileManager.defaultManager().fileExistsAtPath(path) {
+            return (PathStatus.Exists, nil)
+        }
+        
+        do {
+            try NSFileManager.defaultManager().createDirectoryAtPath(folderPath, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            debugPrint(error)
+            return (PathStatus.Error, nil)
+        }
+        
+        return (PathStatus.IsFree, path)
+    }
+
 }
